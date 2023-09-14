77034de270fc347afcb8498c248e11e3a5f9f5a6
이현종 <62270266+Brem0827@users.noreply.github.com>
2023-09-14 11:38:49 +0900

    Update birthday

---
+++ birthday/src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: '이현종',
+    age: 24,
+    image:
+      'https://avatars.githubusercontent.com/u/62270266?v=4?s=100',
+  },
+  {
+    id: 2,
+    name: '정다훈',
+    age: 24,
+    image:
+    'https://avatars.githubusercontent.com/u/62270266?v=4?s=100',
+  },
+  {
+    id: 3,
+    name: '정우성',
+    age: 24,
+    image:
+    'https://avatars.githubusercontent.com/u/62270266?v=4?s=100',
+  },
+];
